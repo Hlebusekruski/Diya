61e9e36377c6fe302d8638bff0851dbeddb33cdd
Hlebusekruski <106579961+Hlebusekruski@users.noreply.github.com>
2024-12-08 20:02:11 +0200

    Add files via upload

---
+++ access/values.js
@@ -0,0 +1,24 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = true; // Права
+var isCovidCertificateEnabled = true; // COVID-сертификат
+
+// Общее
+var birthdate = "21.03.2006"; // Дата рождения
+var fullname = "Авраменко Вікторія Вікторівна" // ФИО
+var name = "Вікторія" // Имя
+
+// Паспорт
+var passport_id = "0976327588" // Номер паспорта
+
+// КПП
+var kpp_id = "0356327567" // ИНН
+
+// Права
+var rights_valid_until = "02.04.2028" // Права "Дійсні до"
+var rights_categories = "A, C, D, T" // Права "Категорії"
+var rights_tsc = "ТСЦ 8631" // Права "Видав
+var rights_id = "KBE968639" // Права "Серія та номер""
+
+// COVID-сертификат
+var covid_valid_until = "01.01.2028" // Сертификат "Дійсний до"
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
